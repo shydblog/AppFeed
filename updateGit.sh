134d237f88eddae93da72b8d28acf09672f497fb
shyd <1104196655@qq.com>
2020-07-19 23:19:41 -0700

    Initial commit

--- updateGit.sh
+++ updateGit.sh
@@ -2,9 +2,9 @@
 cd /tmp/GitHub/AppFeed
 git config --global user.email "1104196655@qq.com"
 git config --global user.name "shyd"
-cd existing_folder
+
 git init
-git remote add origin git@e.coding.net:shyd-unraid/unRAIDServer/AppFeed.git
+
 git add .
 git commit -m "Initial commit"
 git push -u origin master -f
@@ -13,9 +13,9 @@ git push -u origin master -f
 cd /tmp/GitHub/Squidly271.github.io
 git config --global user.email "1104196655@qq.com"
 git config --global user.name "shyd"
-cd existing_folder
+
 git init
-git remote add origin git@e.coding.net:shyd-unraid/unRAIDServer/Squidly271.github.io.git
+
 git add .
 git commit -m "Initial commit"
 git push -u origin master -f
